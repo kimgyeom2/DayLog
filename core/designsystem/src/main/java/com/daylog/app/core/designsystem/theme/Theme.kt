31f2c9fcab4373e347ca3f0310a57a716f2d4c9e
Gyeom <kimgyeom2@gmail.com>
2026-02-13 16:55:10 +0900

    [design-system] DaylogColor 추가

--- core/designsystem/src/main/java/com/daylog/app/core/designsystem/theme/Theme.kt
+++ core/designsystem/src/main/java/com/daylog/app/core/designsystem/theme/Theme.kt
@@ -1,28 +1,23 @@
 package com.daylog.app.core.designsystem.theme
 
-import android.app.Activity
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
-import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.graphics.Color
-import androidx.core.view.WindowCompat
-import com.daylog.app.core.designsystem.R
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Color.DarkGray,
-    secondary =  Color.DarkGray,
-    tertiary =Color.DarkGray
+    primary = DaylogColor.DarkGray,
+    secondary =  DaylogColor.DarkGray,
+    tertiary =DaylogColor.DarkGray,
+    background = DaylogColor.White
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Color.DarkGray,
-    secondary = Color.DarkGray,
-    tertiary = Color.DarkGray,
-    background = Color.DarkGray // 여기에 배경색 지정
+    primary = DaylogColor.White,
+    secondary = DaylogColor.White,
+    tertiary = DaylogColor.White,
+    background = DaylogColor.White
 )
 
 @Composable
